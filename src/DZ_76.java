219b174f1bdf82406fcfa6b5c8782f7ff0a6ae81
rak15 <rak1529916@mail.ru>
2023-05-07 17:03:07 +0300

    1

--- src/DZ_76.java
+++ src/DZ_76.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class DZ_76 {
+import java.util.Scanner;
+
+public class DZ_76 {
+    public static void main(String[] args) {
+        String name, lastName,age1;
+        int age=-1;
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        lastName = scanner.next();
+        age1 = scanner.next();
+            for (int i = 0; i < 1;) {
+                try {
+                age=Integer.parseInt(age1);
+                if (age>-1)
+                    i++;
+                if (age<0){
+                    System.out.println("Возраст не может быть меньше 0");
+                    age1=scanner.next();}
+            } catch (NumberFormatException e) {
+                    System.out.println("Введен некорректный возраст, попробуйте ввести возраст с помощью цифр");
+                    age1=scanner.next();
+                }
+            }
+        scanner.close();
+
+        System.out.printf("Пользователя зовут %s %s. Его возраст - %d",name,lastName,age);
+
+
+    }
 }
